3d482123454b30958d5c7f31a0ce29ec437d767c
Tomás Leggire <tomasleggire@hotmail.com>
2023-09-11 18:47:54 +0000

    Context created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import './styles/App.css';
+import PasswordContext from './contexts/PasswordContext';
 import Header from './components/Header';
 
 export default function App() {
   return (
+    <PasswordContext.Provider value={undefined}>
     <div className="app">
       <Header />      
     </div>
+    </PasswordContext.Provider>
   );
 }
---
+++ src/contexts/PasswordContext.tsx
@@ -0,0 +1,4 @@
+   import { createContext } from 'react';
+   const PasswordContext = createContext(undefined);
+
+   export default PasswordContext;
