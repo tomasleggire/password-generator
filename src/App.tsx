203ab5d4ee0ee79b8fb833489af8f91314388ed0
Tomás Leggire <tomasleggire@hotmail.com>
2023-09-11 18:19:36 +0000

    Header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './styles/App.css';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <div className="app">
-      
+      <Header />      
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/Header.css';
+
+export default function Header(): JSX.Element {
+    return (
+        <><p>Password Generator</p></>
+    )
+}
