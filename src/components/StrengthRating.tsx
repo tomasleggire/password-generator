ed48df78dd48ac24b249957fbf2b78905bf4367e
Tomás Leggire <tomasleggire@hotmail.com>
2023-10-05 19:11:26 +0000

    Strength rating termiando

--- src/components/StrengthRating.tsx
+++ src/components/StrengthRating.tsx
@@ -1,37 +1,56 @@
-import React, { useContext } from 'react';
-import '../styles/StrengthRating.css';
-import { MyContext } from "../contexts/PasswordContext"; 
+import React, { useContext } from "react";
+import "../styles/StrengthRating.css";
+import { MyContext } from "../contexts/PasswordContext";
 
 export default function StrengthRating() {
-    const { newPassword } = useContext(MyContext);
+  const { newPassword } = useContext(MyContext);
 
-    let strength = 0;
+  let strength = 0;
 
-    if (newPassword.length >= 4) {
-      const hasUpperCase = /[A-Z]/.test(newPassword);
-      const hasLowerCase = /[a-z]/.test(newPassword);
-      const hasNumber = /\d/.test(newPassword);
-      const hasSpecialChar = /\W/.test(newPassword);
-  
-      if (hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar) {
-        strength = newPassword.length > 8 ? 3 : 2;
-      } else if (hasUpperCase || hasLowerCase || hasNumber || hasSpecialChar) {
-        strength = 1;
-      }
+  if (newPassword.length >= 4) {
+    const hasUpperCase = /[A-Z]/.test(newPassword);
+    const hasLowerCase = /[a-z]/.test(newPassword);
+    const hasNumber = /\d/.test(newPassword);
+    const hasSpecialChar = /\W/.test(newPassword);
+
+    let count = 0;
+    if (hasUpperCase) count++;
+    if (hasLowerCase) count++;
+    if (hasNumber) count++;
+    if (hasSpecialChar) count++;
+
+    if (count >= 3 && newPassword.length > 12) {
+      strength = 4;
+    } else if (count >= 2 && newPassword.length > 8) {
+      strength = 3;
+    } else if (count >= 2 && newPassword.length <= 8) {
+      strength = 2;
+    } else {
+      strength = 1;
     }
-  
-    return (
-      <div className="strength-rating-container">
-        <h5>STRENGTH</h5>
-        <div className="strength-rating-unity-container">
-        {Array(4).fill("").map((_, i) => (
-          <div key={i} className={`${i < strength ? "strength-rating-unity-filled" : "strength-rating-unity"}`}></div>
-        ))}
-        </div>
+  } else if (newPassword != "") {
+    strength = 1;
+  }
+
+  return (
+    <div className="strength-rating-container">
+      <h5>STRENGTH</h5>
+      <div className="strength-rating-unity-container">
+        {Array(4)
+          .fill("")
+          .map((_, i) => (
+            <div
+              key={i}
+              className={`${
+                i < strength
+                  ? "strength-rating-unity-filled"
+                  : "strength-rating-unity-empty"
+              }`}
+            ></div>
+          ))}
       </div>
-    );
+    </div>
+  );
 }
 
-const styleNotFill = {
-
-}
+const styleNotFill = {};
