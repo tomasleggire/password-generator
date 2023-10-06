e366bc7ea64f02134b3710f812edb645ae57cbf1
Tomás Leggire <tomasleggire@hotmail.com>
2023-10-06 18:34:13 +0000

    Copiar en el clipboard agregado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,29 @@
-import { useContext } from 'react';
-import { MyContext } from '../contexts/PasswordContext';
-import { FaRegCopy } from 'react-icons/fa';
+import { useContext } from "react";
+import { MyContext } from "../contexts/PasswordContext";
+import { FaRegCopy } from "react-icons/fa";
 
-import '../styles/Header.css';
+import "../styles/Header.css";
 
 export default function Header(): JSX.Element {
+  const { initialStatePassword, newPassword } = useContext(MyContext);
 
-    const { initialStatePassword, newPassword } = useContext(MyContext);
+  const handleButton = () => {
+    !newPassword ? "" : navigator.clipboard.writeText(newPassword);
+  };
 
-    const handleButton = () => {
-      !newPassword ? "" : console.log(newPassword);
-    }
-
-    return (
-        <div className="main-header">
-          <h2>Password Generator</h2>
-          <div className="header-main-section">
-            {
-            !newPassword ? 
-            (<p className="header-password-invalid">{initialStatePassword}</p>) : 
-            (<p className="header-password-valid">{newPassword}</p>)
-            }
-            <button type="button" onClick={handleButton}><FaRegCopy className="header-main-button-icon"/></button>
-          </div>
-        </div>
-    )
+  return (
+    <div className="main-header">
+      <h2>Password Generator</h2>
+      <div className="header-main-section">
+        {!newPassword ? (
+          <p className="header-password-invalid">{initialStatePassword}</p>
+        ) : (
+          <p className="header-password-valid">{newPassword}</p>
+        )}
+        <button type="button" onClick={handleButton}>
+          <FaRegCopy className="header-main-button-icon" />
+        </button>
+      </div>
+    </div>
+  );
 }
